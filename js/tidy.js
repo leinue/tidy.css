ab36c9ba26f2e4ab76a8109362d7dde9383aa806
ivy <597055914@qq.com>
2015-06-08 21:39:50 +0800

    update

--- js/tidy.js
+++ js/tidy.js
@@ -1,4 +1,5 @@
 	$(document).ready(function(){
+		/*分割按钮开始*/
 		$('.btn-split li').click(function(){
 			var _this=$(this);
 			var isAllowMultiselect=_this.parent().hasClass('allow-multiselect');
@@ -21,6 +22,7 @@
 
 	});
 
+	/*设置图标*/
 	var setHeaderIcon=function(url,normalSize=20,hoverSize=30){
 		//https://cdn1.iconfinder.com/data/icons/logotypes/32/twitter-128.png
 		$('.tidy-menu ul li.header-icon').css({
@@ -41,6 +43,7 @@
 		});
 	};
 
+	/*二级菜单开始*/
 	$('.tidy-menu ul li').hover(function(){
 		var this_=$(this);
 		var thisNext=this_.find('.tidy-menu-second-level');
@@ -65,3 +68,121 @@
 		}
 	});
 
+
+	/*分页开始*/
+	function removeActive(obj){
+		$(obj).parent().find('.pagination-active').removeClass('pagination-active');
+	}
+
+	function getTotalPages(obj){
+		return $(obj).parent().attr('whole-page');
+	}
+
+	function getCurrentPage(obj){
+		return $(obj).parent().find('.pagination-active').attr('id').split('_')[1];
+	}
+
+	function getFirstpage(obj){
+		return $(obj).parent().find('li:nth-child(3)').html();
+	}
+
+	function getLastPage(obj){
+		return parseInt($(obj).parent().find('li').length)-6;
+	}
+
+	function getNextPage(cp){
+		return parseInt(cp)+1;
+	}
+
+	function setThisPageActive(obj,page){
+		removeActive(obj);
+		$(obj).parent().find('#page_'+page).addClass('pagination-active');
+	}
+
+	function reDrawPage(obj,s,l){
+		l=(l==null)?false:l;
+		if(!l){
+			var start=s;
+		}else{
+			var start=parseInt(s)-parseInt(getLastPage(obj))+1;
+		}
+		$(obj).parent().find('li a').each(function(){
+			if(!isNaN($(this).html())){
+				$(this).html(start);
+				$(this).attr('id','page_'+start);
+				start=parseInt(start)+1;
+			}
+		});
+	}
+
+	function towhichPage(obj,lastPage,nextPage,isLast){
+		isLast=(isLast==null)?false:isLast;
+		var _thisParent=$(obj).parent();
+		if(!isLast){
+			var mid=Math.ceil(Math.ceil(getLastPage(obj))/2);
+			var midPos=mid+2;
+			removeActive(obj);
+			var index=nextPage-mid+1;
+		}else{
+			midPos=getLastPage(obj)+2;
+			var index=lastPage;
+			nextPage=index;
+			removeActive(obj);
+		}
+		reDrawPage(obj,index,isLast);
+		_thisParent.find('li:nth-child('+midPos+')').html('<a class="btn pagination-active" id="page_'+nextPage+'" href="javascript:void(0)">'+nextPage+'</a>');
+	}
+
+	$('.pagination ul li').click(function(){
+		if($(this).find('input').length!=0){return;}
+		var tagAInThis=$(this).find('a');
+		var htmlInA=tagAInThis.attr('id').split('_')[1];
+		if(!isNaN(htmlInA)){
+			removeActive(this);
+			tagAInThis.addClass('pagination-active');
+		}else{
+			switch(htmlInA){
+				case 'home':
+					if($(this).parent().find('#page_1').length==0){
+						var start=1;
+						reDrawPage(this,start);
+						setThisPageActive(this,1);
+					}else{
+						setThisPageActive(this,1);
+					}
+					break;
+				case 'next':
+					var _this=$(this);
+					var _thisParent=_this.parent();
+					var firstPage=getFirstpage(this);
+					var lastPage=getLastPage(this);
+					var currentPage=getCurrentPage(this);
+					var nextPage=getNextPage(currentPage);
+					if(nextPage>=lastPage && nextPage<=getTotalPages(this)){
+						towhichPage(this,lastPage,nextPage);
+					}else{
+						setThisPageActive(this,nextPage);
+					}
+					break;
+				case 'last':
+					var total=getTotalPages(this);
+					var lastPage=getLastPage(this);
+					var nextPage=getNextPage(total);
+					if(total>=lastPage && total<=getTotalPages(this)){
+						towhichPage(this,total,nextPage,true);
+					}else{
+						setThisPageActive(this,total);
+					}
+					break;
+				case 'submit':
+					var pageObtained=$(this).parent().find('input').val();
+					if(pageObtained==''){
+						alert('请输入页数');
+					}else{
+						towhichPage(this,pageObtained,getNextPage(pageObtained)-1);
+					}
+					break;
+			}
+		}
+		// $('html,body').animate({scrollTop:$('#top_pixiv_img').height()-$('.cg_title').height()}, 'slow');
+	});
