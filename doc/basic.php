e4336940d52350cffe4fed3af72c822a67fcc01f
ivy <597055914@qq.com>
2015-06-13 10:54:21 +0800

    update

--- doc/basic.php
+++ doc/basic.php
@@ -2,6 +2,8 @@
 	 <h1>基础入门</h1>
 </div>
 <div class="content">
+	<h3 class="marketing-header">开始说明</h3>
+	<p>我们会尽量用最少的语言帮助你们理解Tidy的使用方法,以减少你们的学习成本.Tidy没有很多的控件(基础控件是有的),他旨在帮助开发者以更少的代码,更轻量的形式完成响应式布局.</p>
 	<h3 class="marketing-header">安装Tidy</h3>
 	<p>安装Tidy只需要引入CSS文件</p>
 	<pre class="code code-wrap code-wrap-full" data-language="html"><code class="rainbow"><span class="support tag"><span class="support tag">&lt;</span><span class="support tag-name">link</spans></span> <span class="support attribute">rel</span><span class="support operator">=</span><span class="string quote">"</span><span class="string value">stylesheet</span><span class="string quote">"</span> <span class="support attribute">href</span><span class="support operator">=</span><span class="string quote">"</span><span class="string value">http://ivydom.com/tidy/0.1.0/tidy-min.css</span><span class="string quote">"</span><span class="support tag close">&gt;</span></code></pre>
